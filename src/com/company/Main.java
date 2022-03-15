423e1a9ffbc323147307e15d4640df5aa62ceb06
Papagoth <ivanmaslov19911@gmail.com>
2022-03-16 00:05:10 +0400

    Master third line added

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	System.out.println("Branch master2");
 	System.out.println("Branch master1 checkout from master");
+    System.out.println("For revert");
     }
 }
