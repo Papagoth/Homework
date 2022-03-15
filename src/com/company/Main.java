cdc02aba4ef77a95fe1dc6a790d4e4239f86e1bb
Papagoth <ivanmaslov19911@gmail.com>
2022-03-16 00:13:23 +0400

    master created index out of range exception

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,21 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
 	System.out.println("Branch master2");
 	System.out.println("Branch master1 checkout from master");
+        List<String> liststr =new ArrayList<>();
+        liststr.add("aaa");
+        liststr.add("aaa");
+        liststr.add("aaa");
+        liststr.add("aaa");
+        for (int i = 0; i < liststr.size();i++)
+        {
+            System.out.println(liststr.get(i).charAt(i));
+        }
     }
 }
