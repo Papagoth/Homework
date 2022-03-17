70ec162637f20f6c1726347f709aad1cf77b4ac1
Papagoth <ivanmaslov19911@gmail.com>
2022-03-18 00:30:33 +0400

    create List and his methods

---
+++ src/com/company/List.java
@@ -0,0 +1,104 @@
+package com.company;
+
+public class List<T> {
+    private Node<T> head;
+    private Node<T> last;
+    private int size = 0;
+    public void add(T t)
+    {
+     Node<T> newNode = new Node<T>(t);
+    if(head == null)
+    {
+      head = newNode;
+      last = newNode;
+    }
+    else
+    {
+        last.setNext(newNode);
+        newNode.setPrev(last);
+        last = newNode;
+    }
+    size ++;
+    }
+    public T get(int index)
+    {
+        Node<T> Head = head;
+        T t;
+        for(int i = 0; i < index-1;i++)
+        {
+            Head = Head.getNext();
+        }
+        return Head.getT();
+    }
+    public void remove(int i)
+    {
+
+        Node<T> Head = head;
+        for(int j = 0; j < i-1;j++)
+        {
+            Head = Head.getNext();
+        }
+        if(Head == head)
+        {
+            head = head.getNext();
+            return;
+        }
+        if(Head == last)
+        {
+            Head.getPrev().setNext(null);
+            last = last.getPrev();
+            return;
+        }
+        Head.getPrev().setNext(head.getNext());
+        Head.getNext().setPrev(head.getPrev());
+        size --;
+    }
+    public void replace(int i, T t)
+    {
+        Node<T> Head = head;
+        for(int j = 0; j < i-1;j++)
+        {
+            Head = Head.getNext();
+        }
+        Head.setT(t);
+    }
+    public int getSize() {
+        return size;
+    }
+
+    public class Node<T> {
+        private Node next;
+        private Node prev;
+        private T num;
+
+        public Node(T n)
+        {
+            this.num = n;
+        }
+
+        public T getT() {
+            return num;
+        }
+
+        public Node getPrev() {
+            return prev;
+        }
+
+        public Node getNext() {
+            return next;
+        }
+
+        public void setT(T num) {
+            this.num = num;
+        }
+
+        public void setPrev(Node prev) {
+            this.prev = prev;
+        }
+
+        public void setNext(Node next) {
+            this.next = next;
+        }
+    }
+
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,11 +1,23 @@
 package com.company;
 
+import java.util.LinkedList;
+import java.util.concurrent.Callable;
+
 public class Main {
 
-    public static void main(String[] args) {
-	System.out.println("Branch master2");
-	System.out.println("Branch master1 checkout from master");
-        System.out.println("Branch master1 after reset");
+
+    public static void main(String[] args)
+    {
+        List<Integer> list =new List<>();
+        list.add(132);
+        list.add(12);
+        list.add(132);
+        list.add(12);
+        list.add(132);
+        list.add(12);
+        list.replace(3,1000);
+        System.out.println(list.get(3));
 
     }
+
 }
